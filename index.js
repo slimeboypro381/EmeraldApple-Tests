0532dbd0e645f2b79e873dc1777cbda3eda22c60
Sebastian Salazar <sebasmike21@gmail.com>
2022-03-25 17:59:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
+    if (message.content == "!"){
         message.reply("Hello World!")
     }
 })
