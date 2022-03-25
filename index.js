8ddc83f7aaaa954ede8f35b75eacc76b2c8f6e3b
Sebastian Salazar <sebasmike21@gmail.com>
2022-03-24 21:41:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js")
+
+const TOKEN = "OTU2NjU5MjMwOTIyOTMyMjg0.YjzcVg.e1QnftUZtTuS_0o61t1xXz45jV8"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log();(`logged in as ${client.user.tag}`)
+})
+
+client.login(TOKEN)
