9c3c8119be2c1f47f6208b39544a0c1b51f40016
Sebastian Salazar <sebasmike21@gmail.com>
2022-03-26 14:10:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const Discord = require("discord.js")
-
-const TOKEN = "OTU2NjU5MjMwOTIyOTMyMjg0.YjzcVg.e1QnftUZtTuS_0o61t1xXz45jV8"
-
-const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES"
-    ]
-})
-
-// notes is here test note 
-
-client.login(TOKEN)
-
 client.on("ready", () => {
     console.log();(`logged in as ${client.user.tag}`);
     client.user.setActivity("Testing 101");
@@ -24,20 +9,5 @@ client.on("messageCreate", (message) => {
     }
 })
 
-client.on("message", async message => {
-    const prefix = "+";
+client.login(TOKEN)
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    if (command === "play") {
-        let track = await client.player.play(message.member.voice.channel, args[0], message.member.user.tag);
-        message.channel.send(`Currently playing ${track.name}! - Requested by ${track.requestedBy}`);
-    }
-
-    if (command === "stop") {
-        let track = await client.player.stop(message.guild.id);
-        message.channel.send("Stopped");
-    }
-
-})
