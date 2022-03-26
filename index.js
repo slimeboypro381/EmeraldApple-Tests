8e4047d3be965df2fb5b843c98a04f465788f5f0
Sebastian Salazar <sebasmike21@gmail.com>
2022-03-26 13:51:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const client = new Discord.Client({
     ]
 })
 
+// notes is here test note 
+
+client.login(TOKEN)
+
 client.on("ready", () => {
     console.log();(`logged in as ${client.user.tag}`);
     client.user.setActivity("Testing 101");
@@ -20,4 +24,20 @@ client.on("messageCreate", (message) => {
     }
 })
 
-client.login(TOKEN)
+client.on("message", async message => {
+    const prefix = "+";
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === "play") {
+        let track = await client.player.play(message.member.voice.channel, args[0], message.member.user.tag);
+        message.channel.send(`Currently playing ${track.name}! - Requested by ${track.requestedBy}`);
+    }
+
+    if (command === "stop") {
+        let track = await client.player.stop(message.guild.id);
+        message.channel.send("Stopped");
+    }
+
+})
