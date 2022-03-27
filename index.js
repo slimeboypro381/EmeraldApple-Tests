c74a87c6b9170a585d723ac65744d647971b5088
Sebastian Salazar <sebasmike21@gmail.com>
2022-03-27 16:46:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const Discord = require("discord.js")
+
+const TOKEN = "OTU2NjU5MjMwOTIyOTMyMjg0.YjzcVg.e1QnftUZtTuS_0o61t1xXz45jV8"
+
 client.on("ready", () => {
     console.log();(`logged in as ${client.user.tag}`);
     client.user.setActivity("Testing 101");
