9acd53a20f7548ac283588e52be8eb02147e9fc8
Sebastian Salazar <sebasmike21@gmail.com>
2022-04-25 17:12:00 -0400

    Revert "Update index.js"
    This reverts commit 90b6882b3b0ace6040174893147851cf0c4eeb56.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TOKEN = "OTU2NjU5MjMwOTIyOTMyMjg0.YjzcVg.e1QnftUZtTuS_0o61t1xXz45jV8"
 
 client.on("ready", () => {
     console.log();(`logged in as ${client.user.tag}`);
-    client.user.setActivity("Testing 101");
+    client.user.setActivity("testing 101");
 })
 
 client.on("messageCreate", (message) => {
